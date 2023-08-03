1f26b075e3befa96a55c183ca5901b1fb46b17f9
RealFictionStudio <realfictionstudio@gmail.com>
2023-08-03 18:48:41 +0200

    Setup script

--- src/go.mod
+++ src/go.mod
@@ -8,7 +8,10 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/fatih/color"
 	"github.com/joho/godotenv"
 )
 
@@ -56,6 +57,15 @@ func main() {
 
 	defer session.Close()
 	fmt.Println("Gobbler is on the wire!")
+	color.Cyan(`
+	  _____       _     _     _           
+	 / ____|     | |   | |   | |          
+   	| |  __  ___ | |__ | |__ | | ___ _ __ 
+   	| | |_ |/ _ \| '_ \| '_ \| |/ _ \ '__|
+   	| |__| | (_) | |_) | |_) | |  __/ |   
+	 \_____|\___/|_.__/|_.__/|_|\___|_|
+	 
+	`)
 	regularSend(session)
 
 	if len(os.Args) > 1 {
